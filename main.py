45ef6327dfcca73dabd05d4f07028402649126cc
Julien Khlaut <57759901+JulienKhlt@users.noreply.github.com>
2025-10-21 17:39:20 +0200

    clean repo (#3)
    Co-authored-by: Corentin Dancette <corentin.dancette@raidium.fr>

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from curia!")
-
-
-if __name__ == "__main__":
-    main()
--- upload.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-run_id=$1
-step=$2
-
-uv run dinov2/open_source/save_model.py --run_id "$run_id" --step "$step"
-uv run dinov2/open_source/save_processor.py --run_id "$run_id"
-cp dinov2/open_source/curia_image_processor.py curia/
-uv run upload_hf.py
